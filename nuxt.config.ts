fa70c52821e4e48cba639577bfc87ceffa265ff0
Richard Nader, Jr. <rmnaderdev@gmail.com>
2024-07-09 01:39:21 -0400

    assets/public folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/main.css'],
+  css: ['~/assets/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <Alert />
-  <div>
+  <div class="flex gap-x-4 items-center">
+    <img class="w-16" src="~/assets/images/Bungus.gif" />
     <h1 class="text-3xl font-bold underline">
       Index Page
     </h1>
